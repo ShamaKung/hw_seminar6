fcd58047f054e839af9bb0315662117d926d073e
ShamaKung <shamaturin@gmail.com>
2022-12-20 05:12:19 +0500

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите размерность массива");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[length];
+int sum = 0;
+int i = 0;
+int j = array.Length - 1;
+
+for (i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 10);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+for (i = 0; i < array.Length / 2; i++)
+{
+    Console.Write(array[i] * array[j] + " ");
+    sum = array[i] * array[j] + sum;
+    j--;
+}
+
+Console.WriteLine();
+
+Console.WriteLine("сумму произведений пар чисел " + sum);
