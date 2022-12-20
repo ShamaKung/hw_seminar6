09613587ba168a3f8d37e2d9a79ef54d94c73441
ShamaKung <shamaturin@gmail.com>
2022-12-20 06:46:38 +0500

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите размерность: ");
+int lenght = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+for (int i = 0; i < lenght; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    int x = int.Parse(Console.ReadLine());
+    if (x > 0) count++;
+}
+Console.WriteLine($"Чисел больше 0: {count}");
